353b0d37cc4286397f3ff4a416cd97cf4e2d7666
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2024-07-05 15:09:27 +0800

    Update Code.py

--- Code.py
+++ Code.py
@@ -3,3 +3,15 @@ text = "https://github.com/Aria-Dolatabadian"
 
 qr_code = pyqrcode.create(text)
 qr_code.svg ("qr.svg",scale = 10)
+
+
+
+import qrcode
+
+text = "https://www.batleylab.net/"
+
+# Generate the QR code
+qr_code = qrcode.make(text)
+
+# Save the QR code as a JPEG file
+qr_code.save("qr_code.jpg", "JPEG")
